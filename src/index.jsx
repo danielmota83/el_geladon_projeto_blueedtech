a3970006ca10d74a88cf29ffbea5210be187933b
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-07-30 14:56:08 -0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from "./components/Home.jsx"
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode><Home/></React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />,
+// <Home/>
+// );
